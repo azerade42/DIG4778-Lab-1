d2ad26f14bc0f9efa86ef1464fc63f0cb5569b4f
Braydon-Leamer <luminati.confirmed@yahoo.com>
2024-08-28 15:55:22 -0400

    Updated Prefabs v2

--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -7,15 +7,32 @@ public class ChessPiece : MonoBehaviour
     [SerializeField] private Vector3 startPosition;
     [SerializeField] private List<Vector3> validMoves;
     [SerializeField] private Sprite icon;
-
-    public enum PieceColor
-    {
-        White,
-        Black
-    }
-
+    [SerializeField] private Sprite altIcon;
+    [SerializeField] private PieceColor color;
+    [SerializeField] private PieceType type;
     private void Start()
     {
         this.GetComponent<SpriteRenderer>().sprite = icon;
+        if(this.color == PieceColor.black)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = altIcon;
+        }
     }
+    public enum PieceColor
+    {
+        white,
+        black
+    }
+
+    public enum PieceType
+    {
+        pawn,
+        knight,
+        bishop,
+        rook,
+        queen,
+        king
+    }
+
+
 }
