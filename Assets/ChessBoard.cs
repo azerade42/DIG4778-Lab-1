c70e1c98fd0d23b8a4d682e96d2e6ace1c4bc68a
Nicholas <56888291+azerade42@users.noreply.github.com>
2024-08-29 20:55:33 -0400

    Generate chess pieces

--- Assets/ChessBoard.cs
+++ Assets/ChessBoard.cs
@@ -1,22 +1,76 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChessBoard : MonoBehaviour
 {
     [SerializeField] private int boardSize = 8;
     [SerializeField] private float tileSize = 1;
+    [field: SerializeField] public List<ChessPiece> chessPieces { get; private set; }
+
+    private Vector3 pieceOffset;
+
+    private List<ChessPiece> currentPieces = new List<ChessPiece>();
+
+
     public void OnDrawGizmos()
     {
-        Gizmos.color = Color.black;
+        GenerateBoard();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        ResetPieces();
+        GeneratePieces();
+    }
+
+    private void GenerateBoard()
+    {
         for (int i = 0; i < boardSize; i++)
         {
             for (int j = 0; j < boardSize; j++)
             {
-                float boardOffset = (float)boardSize/2 - tileSize / 2;
-                Vector3 boardPosition = new Vector3(i - boardOffset, j - boardOffset, 1);
-                Gizmos.DrawCube(boardPosition, Vector3.one);
-                Gizmos.color = SwapColor(Gizmos.color);
+                GenerateBoardTile(i, j);
             }
             Gizmos.color = SwapColor(Gizmos.color);
+        }
+    }
+
+    public void GenerateBoardTile(int i, int j)
+    {
+        float boardOffset = (float)boardSize/2 - tileSize / 2;
+        Vector3 boardPosition = new Vector3(i - boardOffset, j - boardOffset, 1);
+        if (i == 0 && j == 0)
+            pieceOffset = boardPosition;
+        Gizmos.DrawWireCube(boardPosition, Vector3.one * tileSize);
+        Gizmos.color = SwapColor(Gizmos.color);
+    }
+
+    private void ResetPieces()
+    {
+        foreach (ChessPiece piece in currentPieces)
+            DestroyImmediate(piece.gameObject);
+
+        currentPieces.Clear();
+        Gizmos.color = Color.black;
+    }
+
+    private void GeneratePieces()
+    {
+        foreach (ChessPiece piece in chessPieces)
+        {
+            if (piece == null) continue;
+
+            foreach (Vector2 startPosition in piece.startPositions)
+            {
+                Vector3 piecePosition = pieceOffset + new Vector3(startPosition.x, startPosition.y, 0);
+                ChessPiece newPiece = Instantiate(piece, piecePosition, Quaternion.identity, transform).GetComponent<ChessPiece>();
+                currentPieces.Add(newPiece);
+                newPiece.SwapPieceColor();
+                
+                newPiece.gameObject.name = Enum.GetName(typeof(ChessPiece.PieceType), newPiece.type);
+            }
         }
     }
 
--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -1,38 +1,112 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChessPiece : MonoBehaviour
 {
-    [SerializeField] private Vector3 startPosition;
-    [SerializeField] private List<Vector3> validMoves;
+    [field: SerializeField] public List<Vector2> startPositions { get; set; }
+    [SerializeField] private List<Vector2> validMoves;
     [SerializeField] private Sprite icon;
-    [SerializeField] private Sprite altIcon;
     [SerializeField] private PieceColor color;
-    [SerializeField] private PieceType type;
-    private void Start()
-    {
-        this.GetComponent<SpriteRenderer>().sprite = icon;
-        if(this.color == PieceColor.black)
-        {
-            this.GetComponent<SpriteRenderer>().sprite = altIcon;
-        }
-    }
+    [field: SerializeField] public PieceType type { get; set; }
+
     public enum PieceColor
     {
         white,
         black
     }
-
     public enum PieceType
     {
-        pawn,
-        knight,
-        bishop,
-        rook,
-        queen,
-        king
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
     }
 
+    private void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sprite = icon;
+        SwapPieceColor();
+    }
 
+    public void SwapPieceColor()
+    {
+        if(this.color == PieceColor.black)
+        {
+            this.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        SwitchPiece();
+        Gizmos.color = Color.blue;
+        DrawValidMoveRays();
+    }
+
+    private void DrawValidMoveRays()
+    {
+        for (int i = 0; i < validMoves.Count; i++)
+        {
+            Vector2 move = validMoves[i];
+            Vector3 pieceMove = new Vector3(move.x, move.y, 0);
+
+            if (type == PieceType.Knight)
+            {
+                bool xGreater = Mathf.Abs(move.x) > Mathf.Abs(move.y);
+                Vector2 ray1, ray2;
+
+                if (xGreater)
+                {
+                    ray1 = new Vector2(move.x, 0);
+                    ray2 = new Vector2(0, move.y);
+                }
+                else
+                {
+                    ray1 = new Vector2(0, move.y);
+                    ray2 = new Vector2(move.x, 0);
+                }
+                
+                Gizmos.DrawRay(transform.position, ray1);
+                Gizmos.DrawRay((Vector2)transform.position + ray1, ray2);
+            }
+            else if (type == PieceType.Pawn)
+            {
+                float offset = 0.1f;
+                Vector2 offsetVector = new Vector2(offset, 0);
+                if (i == 0)
+                {
+                    Gizmos.DrawRay((Vector2)transform.position - offsetVector, (Vector2)pieceMove);
+                }
+                else
+                {
+                    Gizmos.DrawRay((Vector2)transform.position + offsetVector, (Vector2)pieceMove);
+                }
+            }
+            else
+            {
+                Gizmos.DrawRay(transform.position, pieceMove);
+            }
+        }
+    }
+
+    private void SwitchPiece()
+    {
+        foreach (ChessPiece piece in transform.parent.GetComponent<ChessBoard>().chessPieces)
+        {
+            if (piece.type == this.type)
+            {
+                this.startPositions = piece.startPositions;
+                this.validMoves = piece.validMoves;
+                this.icon = piece.icon;
+                GetComponent<SpriteRenderer>().sprite = icon;
+                SwapPieceColor();
+                gameObject.name = Enum.GetName(typeof(PieceType), piece.type);
+            }
+        }
+    }
+    
 }
